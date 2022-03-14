4a497056080eb55024d4c28527e1db882659876f
Rajveer <93126793+Rajveer2009@users.noreply.github.com>
2022-03-14 10:18:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+window.aler("Are you over the age of 18 if yes you may proceed if not DO NOT continue /n rated A")
+
 function genrateNum() {
     let num = Math.floor(Math.random() * document.getElementById("number").value);
     document.getElementById("numeberOutput").innerHTML = num;
